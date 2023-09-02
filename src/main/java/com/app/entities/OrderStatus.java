7c51ac5e18ad2e4a0b02b88fb1c20fe0530f25a2
RathodPawan <129750902+RathodPawan@users.noreply.github.com>
2023-09-02 12:19:07 +0530

    Delete src directory

--- src/main/java/com/app/entities/OrderStatus.java
+++
@@ -1,6 +0,0 @@
-package com.app.entities;
-
-public enum OrderStatus 
-{
-	ACCEPTED,PLACED,PACKING,READY,OUT_FOR_DELIVERY,DELIVERED,CANCELLED
-}
--- src/main/java/com/app/service/ICategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.app.service;
-
-import com.app.entities.Category;
-
-public interface ICategoryService {
-	
-public Category addCategory(Category cat);
-
-}
